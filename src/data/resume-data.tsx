b04cb554f4b4ef4ff5fbb5e8e1b784f7e98eb919
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2024-01-26 14:04:00 +0100

    Modify resume-data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,189 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Nico Wickersheim",
+  initials: "NW",
+  location: "Freiburg, Germany",
+  locationLink: "https://maps.app.goo.gl/wPQbwzPRPN5H9V4X8",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Passionated Full Stack Engineer with focus on Kotlin with Ktor, React and Cloud-Native in an agile environment.",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "As a Full Stack Engineer, I have been working on a wide range of projects. Gained experience in building and maintaining process-driven ERP systems, now working with financial web applications. I am a big fan of Kotlin and React. I am also interested in Cloud-Native technologies like Terraform, AWS and DevOps.",
+  avatarUrl:
+    "https://avatars.githubusercontent.com/u/35769900?s=400&u=8d47f586b3c540183875fffa3983953934e6966a&v=4",
+  personalWebsiteUrl: "https://nicowickersheim.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "nico.wickersheim@yahoo.de",
+    //tel: "+123456789",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/wickenico",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/nico-wickersheim/",
         icon: LinkedInIcon,
       },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+      // {
+      //   name: "X",
+      //   url: "https://x.com/BartoszJarocki",
+      //   icon: XIcon,
+      // },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school:
+        "Cooperative State University Baden-Württemberg - Center for Advanced Studies",
+      degree: "Master's degree in Business Informatics - Software Engineering",
+      start: "2018",
+      end: "2020",
+    },
+    {
+      school: "Cooperative State University Baden-Württemberg",
+      degree:
+        "Bachelor's degree in Business Informatics - Software Engineering",
+      start: "2014",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "sevDesk GmbH",
+      link: "https://sevdesk.de",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      title: "Full Stack Developer",
+      start: "May 2022",
+      end: "today",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Creating central services for the sevDesk ecosystem with Kotlin and building the groundworking infrastructure for the future of sevDesk with Terraform and AWS. Working in an agile environment with Scrum and Jira.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      company: "trend SWM GmbH",
+      link: "https://trendswm.de/",
+      badges: ["on-premise"],
+      title: "Software Developer",
+      start: "August 2014",
+      end: "April 2022",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Developing and maintaining process-driven ERP system for the different industries with ILE PRG in an IBM ecosystem.",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
+    "Kotlin",
+    "Ktor",
+    "Docker",
+    "AWS",
+    "Terraform",
+    "Git",
+    "React",
     "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "SQL",
+    "Scrum",
+    "DevOps",
+    "Jira",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Madagassisch.de",
+      techStack: ["Side Project", "React", "Node.js", "Firebase", "NoSQL"],
+      description:
+        "German - Malagasy dictionary built with React and Firebase.",
+      link: {
+        label: "madagassisch.de",
+        href: "https://madagassisch.de/",
+      },
+    },
+    {
+      title: "nicowickersheim.dev",
+      techStack: ["Side Project", "JavaScript", "Bootstrap", "Portfolio"],
+      description: "Developer portfolio built with Bootstrap and JavaScript.",
+      link: {
+        label: "nicowickersheim.dev",
+        href: "https://nicowickersheim.dev/",
+      },
+    },
+    {
+      title: "PyHtmlToCSVConverter",
+      techStack: ["Side Project", "Python", "CSV", "Converter", "Crawling"],
+      description:
+        "Reading data from website and converting it to CSV file using Python.",
+      link: {
+        label: "PyHtmlToCSVConverter",
+        href: "https://github.com/wickenico/py-html-to-csv-converter",
+      },
+    },
+    {
+      title: "Bookify Ktor Backend",
       techStack: [
         "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Kotlin",
+        "Ktor",
+        "REST",
+        "PostgreSQL",
+        "Google Books API",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Book management backend built with Kotlin and Ktor.",
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "bookify-ktor-backend",
+        href: "https://github.com/wickenico/bookify-ktor-backend",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Bookify Vuejs Frontend",
+      techStack: ["Side Project", "Vue", "Node.js"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Book management frontend built with Vue.js and Node.js. Consuming Bookify Ktor Backend.",
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "bookify-vuejs-frontend",
+        href: "https://github.com/wickenico/bookify-vuejs-frontend",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Scriptable BTC-USD Widget",
       techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Side Project",
+        "Scriptable",
+        "JavaScript",
+        "Widget",
+        "Bitcoin",
+        "Crypto",
       ],
       description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+        "Bitcoin price widget for iOS built with Scriptable and JavaScript.",
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "btc-usd-course.js",
+        href: "https://github.com/wickenico/btc-usd-course.js",
       },
     },
     {
-      title: "Evercast",
+      title: "Scriptable Crypto Ticker",
       techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Side Project",
+        "Scriptable",
+        "JavaScript",
+        "Widget",
+        "Crypto",
+        "Coinbase",
       ],
       description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+        "Crypto price widget for iOS built with Scriptable and JavaScript",
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "crypto-ticker-widget.js",
+        href: "https://github.com/wickenico/crypto-ticker-widget.js",
       },
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
+      title: "Scriptable Hub",
+      techStack: ["Side Project", "Scriptable", "JavaScript", "Widget"],
       description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+        "Collection of widgets for iOS built with Scriptable and JavaScript.",
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
+        label: "scriptable-hub",
+        href: "https://github.com/wickenico/scriptable-hub",
       },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
